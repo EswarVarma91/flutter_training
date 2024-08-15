49085a0ed93ffac149e574b16baeace4d12b90b0
Eswar Varma Panumetcha <abc@gmail.com>
2024-08-15 18:00:53 +0530

    app bar

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -10,9 +10,43 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar: AppBar(title: const Text("My Home Page"),),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        // leading: const Center(
+        //   child: Icon(Icons.phone),
+        // ),
+        // elevation: 5,
+        // centerTitle: true,
+        // actions: [
+        //   InkWell(
+        //       onTap: () {
+        //         print("qr code");
+        //       },
+        //       child: const Icon(Icons.qr_code)),
+        //   const SizedBox(
+        //     width: 15,
+        //   ),
+        //   InkWell(
+        //       onTap: () {
+        //         print("camera");
+        //       },
+        //       child: const Icon(Icons.camera_alt_outlined)),
+        //   const SizedBox(
+        //     width: 15,
+        //   ),
+        //   const Icon(Icons.menu)
+        // ],
+        title: const Text(
+          "My Home Page",
+        ),
+      ),
+      body: const Column(
+        children: [
+          Text("adwfgegr"),
+          Text("fscfergceg"),
+        ],
+      ),
     );
   }
 }
-
